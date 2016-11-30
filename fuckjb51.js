5f211d6eb270aaa6b5fbf715a592a5abfa2fedc3
zhangxingwang <zhangxingwang@sinosoft.com.cn>
2016-11-30 08:58:12 +0800

    添加添加过滤地址后的消息提醒及reload

--- fuckjb51.js
+++ fuckjb51.js
@@ -10,11 +10,15 @@
 // @grant             GM_getValue
 //@grant              GM_deleteValue
 // ==/UserScript==
+
 (function() {
     'use strict';
+ 
     var re="www.jb51.net\/.*?";
     var getRe = GM_getValue('re', re);
+    
     var reg = new RegExp(getRe,"g");
+
     //var resultQuery=document.getElementsByClassName('c-container');
        for(var i=1;i<11;i++)
        {
@@ -37,16 +41,16 @@
     filterButton.setAttribute('type','submit');
     filterButton.setAttribute('value','过滤');
     filterButton.setAttribute('id','filter');
-    //var inputFilter="<input id='inputFilter' placeholder='过滤域名'></input>";
     s_tab.appendChild(inputFilter);
     s_tab.appendChild(filterButton);
   document.getElementById("filter").addEventListener("click", filter );
     function filter(){
     var inputFilter=document.getElementById('inputFilter');
-       // console.log(inputFilter.getAttribute('value'));
          re=re+"|"+inputFilter.value+"\/.*?";
         getRe=re;
         GM_setValue('re',getRe);
-       
+         alert('已添加过滤地址');
+        window.location.reload(true);
     }
+    // Your code here...
 })();
