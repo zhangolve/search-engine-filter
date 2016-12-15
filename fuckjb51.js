857bd8ae71832de985a8214aae06bea1f6e349df
zhangxingwang <zhangxingwang@sinosoft.com.cn>
2016-12-15 19:22:32 +0800

    添加observer监听dom树，去除原有的二次reload方法

--- fuckjb51.js
+++ fuckjb51.js
@@ -1,69 +1,89 @@
 // ==UserScript==
 // @name         fuckjb51
 // @namespace    https://github.com/zhangolve/fuckjb51
-// @version      0.1
-// @description  filter something you don't like
+// @version      0.2
+// @description  filter something you don't like on baidu
 // @author       zhangolve contact   zhangolve@gmail.com
 // @license      GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
 // @match        https://www.baidu.com/*
 // @grant             GM_setValue
 // @grant             GM_getValue
 // @grant              GM_deleteValue
-// @grant       GM_addStyle
+// @grant              GM_addStyle
 // ==/UserScript==
-var searchUrl = 'https://www.baidu.com/s?ie=utf-8&f=8&wd=';
-if (window.location.search !== '') {
-  var re = 'www.open-open.com\/.*?';
 
-  var getRe = GM_getValue('re', re);
-  var reg = new RegExp(getRe);
-  var items = [];
-  var urls = [];
-  for (var i = 1; i < 11; i++) {
-    items.push(document.getElementById(i));
-    var showurl = items[i - 1].getElementsByClassName('c-showurl');
-    if (showurl.length !== 0) {
-      urls.push(items[i - 1].getElementsByClassName('c-showurl')[0].innerHTML);
-      console.log(urls[i - 1]);
-      if (reg.test(urls[i - 1])) {
-        items[i - 1].remove();
-      }
-    }
-  }
-  var button = document.getElementById('su');
-  button.addEventListener('click', timeout);
-  document.addEventListener('keypress', enter);
-  var s_tab = document.getElementById('s_tab');
-  var inputFilter = document.createElement('input');
-  inputFilter.setAttribute('id', 'inputFilter');
-  inputFilter.setAttribute('placeholder', '过滤域名');
-  var filterButton = document.createElement('input');
-  filterButton.setAttribute('type', 'submit');
-  filterButton.setAttribute('value', '过滤');
-  filterButton.setAttribute('id', 'filter');
-  s_tab.appendChild(inputFilter);
-  s_tab.appendChild(filterButton);
-  document.getElementById('filter').addEventListener('click', filter);
+//加入监听DOM树，使该脚本能够应对百度网站上的异步加载
+
+var MObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+var observer = new MObserver(function(records){
+block();
+addStyle();
+});
+var option = {
+'childList': true,
+
+};
+observer.observe(document.body, option);
+
+//block函数用来进行屏蔽
+function block() {
+
+//当在search页面上时才进行加载脚本，防止在其他百度页面出现问题  
+if (window.location.search !== '') {
+
+var re = 'www.open-open.com\/.*?';
+
+var getRe = GM_getValue('re', re);
+var reg = new RegExp(getRe);
+var items = [];
+var urls = [];
+for (var i = 1; i < 11; i++) {
+items.push(document.getElementById(i));
+var showurl = items[i - 1].getElementsByClassName('c-showurl');
+
+if (showurl.length !== 0) {
+urls.push(items[i - 1].getElementsByClassName('c-showurl')[0].innerHTML);
+if (reg.test(urls[i - 1])) {
+console.log(urls[i-1]);  
+items[i - 1].remove();
 }
+}
+}
+
+//添加一个自定义屏蔽网站添加栏，用于添加自定义的屏蔽网站
+
+var button = document.getElementById('su');
+var s_tab = document.getElementById('s_tab');
+if(document.getElementById('inputFilter')===null)
+{
+var inputFilter = document.createElement('input');
+inputFilter.setAttribute('id', 'inputFilter');
+inputFilter.setAttribute('placeholder', '过滤域名');
+var filterButton = document.createElement('input');
+filterButton.setAttribute('type', 'submit');
+filterButton.setAttribute('value', '过滤');
+filterButton.setAttribute('id', 'filter');
+s_tab.appendChild(inputFilter);
+s_tab.appendChild(filterButton);
+document.getElementById('filter').addEventListener('click', filter);
+}
+
+}
+}
+
 function filter() {
-  var inputFilter = document.getElementById('inputFilter');
-  if (inputFilter.value !== '') {
-    getRe = getRe + '|' + inputFilter.value + '\/.*?';
-    GM_setValue('re', getRe);
-    alert('已添加过滤地址');
-    window.location.reload(true);
-  } else {
-    alert('请输入需要过滤的域名');
-  }
+var inputFilter = document.getElementById('inputFilter');
+if (inputFilter.value !== '') {
+getRe = getRe + '|' + inputFilter.value + '\/.*?';
+GM_setValue('re', getRe);
+alert('已添加过滤地址');
+window.location.reload(true);
+} else {
+alert('请输入需要过滤的域名');
 }
-function timeout() {
-  var kw = document.getElementById('kw').value;
-  searchUrl += kw;
-  document.location.href = searchUrl;
 }
-function enter(event) {
-  if (event.keyCode == 13) {
-    this.timeout();
-  }
-}
+
+function addStyle(){
 GM_addStyle('#inputFilter {width: 140px;height:25px;font-size:14px} #filter{width:40px;height:25px;color:red} ');
+}
+
